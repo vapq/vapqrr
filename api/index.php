e114d3dff160a35cbd2890459b9cae6feb7aee57
vodls <vodlsreal@gmail.com>
2021-01-29 13:19:33 -0500

    s

--- api/index.php
+++ api/index.php
@@ -3,9 +3,9 @@
  <h1>Vapqrr's api page that he took 2 minutes on.</h1>
 <a href="dashboard.php">Dashboard</a>
 <hr />
- <a href="/auth">Auth-Test</a>
+ <a href="auth">Auth-Test</a>
 <hr />
- <a href="/bypass">Bypass</a>
+ <a href="bypass">Bypass</a>
 <hr />
  
 </body>
