89f8445a0546fb79c989e7996643271638f7869c
vodls <vodlsreal@gmail.com>
2021-01-29 11:34:52 -0500

    sex

---
+++ blog/index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://blog.lukgth.cloud/main.css" type="text/css">
+<link rel="stylesheet" href="https://blog.lukgth.cloud/blog.css" type="text/css">
+<title>vapqrr's blog</title>
+
+</head><body>
+
+<div id="divbodyholder">
+
+<div class="headerholder"><div class="header">
+
+<div id="title">
+
+<h1 class="nomargin"><a class="ablack" href="">vapqrr's blog</a></h1>
+
+<div id="description">a blog about random shit</div>
+
+</div></div></div>
+
+<div id="divbody"><div class="content">
+
+<h3><a class="ablack" href="h1setting-up-a-mailserverh1.html">
+<h1>hi</h1>
+
+</a></h3>
+
+<div class="subtitle">January 29, 2021 &mdash; 
+avery
+</div>
+<!-- text begin -->
+
+<p>ok, first blog post! going good, i guess :shrug:</p>
