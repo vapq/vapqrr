d5b4618f6d728dea52fefcd7ec6673d59bf378af
vapq <77282457+vapq@users.noreply.github.com>
2021-01-29 08:45:00 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,11 @@
 <html>
 <body>
+ <h1>Vapqrr's api page that he took 2 minutes on.</h1>
 <a href="dashboard.php">Dashboard</a>
 <hr />
- <a href="dashboard.php">Dashboard</a>
+ <a href="/auth">Auth-Test</a>
 <hr />
- <a href="dashboard.php">Dashboard</a>
+ <a href="/bypass">Bypass</a>
 <hr />
  
 </body>
